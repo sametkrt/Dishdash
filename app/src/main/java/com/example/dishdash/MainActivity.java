42e6c311975b29ed37a3160ce42e900c17fb29a8
sametkrt <omersametk@gmail.com>
2024-12-12 21:34:15 +0000

    Initial button interactions implemented

--- app/src/main/java/com/example/dishdash/MainActivity.java
+++ app/src/main/java/com/example/dishdash/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.dishdash;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton recipeBookButton;
+    ImageButton aiRecipesButton;
+    ImageButton ingredientsButton;
+    ImageButton shoppingListButton;
+    Button sideMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // BUTTONS
+        recipeBookButton = (ImageButton) findViewById(R.id.recipeBookButtonId);
+        aiRecipesButton = (ImageButton) findViewById(R.id.aiRecipesButtonId);
+        ingredientsButton = (ImageButton) findViewById(R.id.ingredientsButtonId);
+        shoppingListButton = (ImageButton) findViewById(R.id.shoppingListButtonId);
+        sideMenuButton = (Button) findViewById(R.id.sideMenuButtonId);
+
+        recipeBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Recipe Book", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        aiRecipesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "AI Recipes", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ingredientsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Ingredients", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        shoppingListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Shopping List", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sideMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "SkMin KeYfInE gOrE bUtOnU", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
