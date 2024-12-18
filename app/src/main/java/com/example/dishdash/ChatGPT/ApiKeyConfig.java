1fd8b4937fdd52690ebdacafe8de5f26c9dfbf7f
sametkrt <omersametk@gmail.com>
2024-12-18 20:03:22 +0000

    Testing

--- app/src/main/java/com/example/dishdash/ChatGPT/ApiKeyConfig.java
+++ app/src/main/java/com/example/dishdash/ChatGPT/ApiKeyConfig.java
@@ -1,5 +1,5 @@
 package com.example.dishdash.ChatGPT;
 
 public class ApiKeyConfig {
-    public static final String API_KEY = ""; // Put the API KEY here for testing - this will be fixed later
+    public static final String API_KEY = " "; // Put the API KEY here for testing - this will be fixed later
 }
